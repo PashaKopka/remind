f2ca7c04dc21e858e2caa10d8ae858c1a6bac5b0
PashaKopka <pashakopka216@gmail.com>
2020-09-26 13:45:55 +0300

    MySQL data base

--- remind/settings.py
+++ remind/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'remind.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'remind',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'DATABASE_HOST': 'localhost'
     }
 }
 
